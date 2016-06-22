17c22b5129f375659b6bc6c7a3d51238fdba1993
gyfus815 <gyfus815@fnikorea.com>
2016-06-22 12:23:59 +0900

    change version
    change

---
+++ Unity/Assets/RotatingObject.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotatingObject : MonoBehaviour {
+
+    private Transform thisTransform;
+    private float speed = 20;
+
+    void Start () {
+        thisTransform = GetComponent<Transform>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        thisTransform.Rotate(Vector3.right * Time.deltaTime * speed);
+        thisTransform.Rotate(Vector3.up * Time.deltaTime * speed, Space.World);
+    }
+}
